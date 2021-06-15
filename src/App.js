a4c511d880d81c6597b1097afef5a5451d81b300
Anthony Palumbo <anthonypalumbo82@gmail.com>
2021-06-15 18:22:51 +0900

    Cleaned up variables.

--- src/App.js
+++ src/App.js
@@ -5,31 +5,24 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
-// import io from 'socket.io-client';
 // import { useCollectionData } from 'react-firebase-hooks/firestore';
-
+require('dotenv').config();
+console.log('pre')
 firebase.initializeApp({
-  apiKey: "AIzaSyAxWR-ulnK5Q4ipQNZ2RbjqmkY1QADeWmo",
-  authDomain: "training-tomo.firebaseapp.com",
-  projectId: "training-tomo",
-  storageBucket: "training-tomo.appspot.com",
-  messagingSenderId: "891311506929",
-  appId: "1:891311506929:web:80c541f38899087720f050"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 });
-
+console.log('post')
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 
 function App() {
   const [user] = useAuthState(auth);
-  // console.log("pre-socket")
-  // const socket = io('ws://localhost:8080');
-  // socket.on('message', text => {
-  //   console.log(text);
-  // });
-  // console.log("post-socket")
-  // socket.emit('message', "HI THERE SOCKET!!!")
   return (
     <div className="App">
       {/* <header className="App-header"> */}
