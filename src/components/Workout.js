40df5e7357106b69b5c39ea486fac38c97eda18c
Anthony Palumbo <anthonypalumbo82@gmail.com>
2021-08-12 12:36:36 +0900

    Cleaned up Workout.js

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -5,26 +5,12 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 
 export default function Workout(props) {
-  /* props.workout should have all the workout data as follows
-  {
-    id: (workout id)
-    name: (Workout Name)
-    available: (boolean false if active)
-    exercises: [
-      {
-        name: (first exercise name)
-        reps: (number of repetitions)
-      },
-      ...
-      {
-        name: (last exercise name)
-        reps: (number of repetitions)
-      }
-    ]
-  }
-  */
+  const {
+    pickWorkout,
+    workout,
+  } = props;
 
-  const exercises = props.workout.exercises.map((e, i) => (
+  const exercises = workout.exercises.map((e, i) => (
     <Row key={i}>
       <Col>
         <Card.Text>{e.name}</Card.Text>
@@ -38,13 +24,13 @@ export default function Workout(props) {
   return (
       <Card style={{ color: "black" }} className="m-2">
         <Card.Header>
-          <Card.Title>{props.workout.name}</Card.Title>
+          <Card.Title>{workout.name}</Card.Title>
         </Card.Header>
         <Card.Body>
           {exercises}
         </Card.Body>
         <Card.Footer>
-          <Button id={props.workout.id} variant="success" onClick={(e) => props.pickWorkout(e.target.id)}>Join Workout</Button>
+          <Button id={workout.id} variant="success" onClick={(e) => pickWorkout(e.target.id)}>Join Workout</Button>
         </Card.Footer>
       </Card>
   )
