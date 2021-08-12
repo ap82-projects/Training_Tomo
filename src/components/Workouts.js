c99dfb9d07b4a78b83b3b13e85245ce297b8ca1a
Anthony Palumbo <anthonypalumbo82@gmail.com>
2021-08-12 12:43:22 +0900

    Cleaned up Workouts.js

--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -1,33 +1,34 @@
 import React, { useState, useEffect } from "react"
-// import Button from "react-bootstrap/Button"
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import UserHeader from './UserHeader';
 import Workout from './Workout';
-// import ChooseWorkout from './ChooseWorkout';
 
 export default function Workouts(props) {
-  const { pickWorkout, socket } = props;
+  const {
+    pickWorkout,
+    socket
+  } = props;
   const [workouts, setWorkouts] = useState([]);
   
+  useEffect(() => {
+    socket.on('getWorkouts', data => {
+      if (data) setWorkouts(data);
+    });
+    console.log('Workouts')
+    console.log(workouts)
+  })
   socket.on('getWorkouts', data => {
     if (data) {
       setWorkouts(data);
     };
   });
-  console.log('Workouts')
-  console.log(workouts)
-
   
   const availableWorkouts = workouts
-  .filter(e => e.available)
-  .map(e => (
-    <Workout pickWorkout={pickWorkout} workout={e} key={e.id} />
-    ))
-    
-    const windowHeight = window.innerHeight;
-    const window80 = Math.floor(windowHeight * .8)
-    return (
-      <div style={{overflow: "scroll", height: `${window80}px` }}>
+    .filter(e => e.available)
+    .map(e => <Workout pickWorkout={pickWorkout} workout={e} key={e.id} />)
+  const windowHeight = window.innerHeight;
+  const window80 = Math.floor(windowHeight * .8)
+  return (
+    <div className="Workouts" style={{overflow: "scroll", height: `${window80}px` }}>
       <h3>Available Workouts</h3>
       {availableWorkouts}
     </div>
